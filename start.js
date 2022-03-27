68caa0749780aa4145a294a90f52107ba9f98ed4
AnishNandi24 <102163207+brogrammer24@users.noreply.github.com>
2022-03-28 00:39:33 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,6 @@
+document.getElementById("loginForm").addEventListener("submit",(event)=>{event.preventDefault()})
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        location.replace("welcome.html")
+    }
+})
