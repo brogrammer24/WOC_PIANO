f90e8ce384b0f2c5b047f3af5d16314e49b03dce
AnishNandi24 <102163207+brogrammer24@users.noreply.github.com>
2022-04-02 12:55:26 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC1kVpTV0vps-LlvDImIX-poSgSaPt2Xcc",
+    authDomain: "piano-4013b.firebaseapp.com",
+    projectId: "piano-4013b",
+    storageBucket: "piano-4013b.appspot.com",
+    messagingSenderId: "819729548383",
+    appId: "1:819729548383:web:f0d04f5f3aa17a967887ea",
+    measurementId: "G-GYB06T91MD"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
