4867ab7737f60ecfcf700f7e2a999afd633db834
AnishNandi24 <102163207+brogrammer24@users.noreply.github.com>
2022-04-02 12:50:26 +0530

    Update and rename start.js to index.js

--- start.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit",(event)=>{event.preventDefault()})
-firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        location.replace("welcome.html")
-    }
-})
